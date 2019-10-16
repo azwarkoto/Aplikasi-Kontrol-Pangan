1f70adbf4c5bef848374482ecac180cabbbddf47
bimosyah <bimosyahputro88@gmail.com>
2019-10-16 23:35:51 +0700

    crud komoditi

--- application/controllers/Komoditi.php
+++ application/controllers/Komoditi.php
@@ -3,18 +3,30 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Komoditi extends CI_Controller {
-
-    
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Komoditi');
+        $this->load->model('mKomoditi','komoditi');
     }
     
-
     public function index()
     {
+        $data['komoditi'] = $this->komoditi->getAll();
         echo "halo";
+    }
+
+    public function save(){
+        $nama = $this->input->post('nama');
+        $data = array("nama" =>$nama);
+        $this->komoditi->save($data);
+    }
+
+    public function delete($id){
+        $this->komoditi->delete($id);
+    }
+
+    public function edit($id){
+        $this->komoditi->edit($id,$data);
     }
 
 }
--- application/models/mKomoditi.php
+++ application/models/mKomoditi.php
@@ -2,32 +2,32 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Komoditi extends CI_Model {
+class mKomoditi extends CI_Model {
     
     public function save($nama){
-        $query = $this->db->insert('komoditi', $nama);
+        $query = $this->db->insert('table_komoditi', $nama);
         return $query;
     }
 
     public function delete($id){
-        $query = $this->db->delete('komoditi');
+        $query = $this->db->delete('table_komoditi');
         return $query;
     }
 
     public function get($id){
         $this->db->where('id', $id);
-        $query = $this->db->get('komoditi');
+        $query = $this->db->get('table_komoditi');
         return $query->result();
     }
 
     public function getAll(){
-        $query = $this->db->get('komoditi');
+        $query = $this->db->get('table_komoditi');
         return $query->result();
     }
 
     public function edit($id,$data){
         $this->db->where('id', $id);
-        $query = $this->db->update('komoditi', $data);
+        $query = $this->db->update('table_komoditi', $data);
         return $query;
     }
 }
